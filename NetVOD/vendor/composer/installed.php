6f10147974122f12df8e29ab7bde4ba45a60c187
Nurd_Enzo <poirsonenzo@gmail.com>
2025-11-03 15:14:23 +0100

    vendor

---
+++ NetVOD/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '655130ae6217947bdfffba78e1a65f81db8265c6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '655130ae6217947bdfffba78e1a65f81db8265c6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
