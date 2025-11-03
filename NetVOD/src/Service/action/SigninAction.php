e30f5256af090abae4006a4d37f4540fac9afed8
Nurd_Enzo <poirsonenzo@gmail.com>
2025-11-03 17:01:52 +0100

    modif form SignInAction

--- NetVOD/src/Service/action/SigninAction.php
+++ NetVOD/src/Service/action/SigninAction.php
@@ -11,7 +11,7 @@ class SigninAction extends Action
             return <<<HTML
             <div class="max-w-md mx-auto p-6 bg-white rounded shadow-md">
                 <h2 class="text-2xl font-bold mb-4">Connexion</h2>
-                <form method="post" action="?action=signin" class="space-y-4">
+                <form method="post" action="?action=SignIn" class="space-y-4">
                     <div>
                         <label for="email" class="block font-semibold">Email :</label>
                         <input type="email" id="email" name="email" required class="w-full border px-3 py-2 rounded">
