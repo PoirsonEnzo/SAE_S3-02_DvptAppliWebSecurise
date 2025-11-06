2f42d0c082f70e96a23138285def3cc1090071d4
MatteoGren <matteog57@outlook.fr>
2025-11-06 11:06:48 +0100

    css + décoration

--- NetVOD/src/Service/action/profil/ChoisirProfilAction.php
+++ NetVOD/src/Service/action/profil/ChoisirProfilAction.php
@@ -9,7 +9,15 @@ class ChoisirProfilAction extends Action
     public function getResult(): string
     {
         if (!isset($_SESSION['user'])) {
-            return "<p>Vous devez être connecté pour choisir un profil.</p>";
+            return <<<HTML
+    <div class="center-message">
+        <h2>Il faut se connecter pour choisir un profil.</h2>
+        <div class="btn-container">
+            <a href="?action=SignIn" class="btn-center">Se connecter</a>
+            <a href="?action=AddUser" class="btn-center">S’inscrire</a>
+        </div>
+    </div>
+HTML;
         }
 
         $pdo = DeefyRepository::getInstance()->getPDO();
