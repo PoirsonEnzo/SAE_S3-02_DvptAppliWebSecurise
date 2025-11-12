7e51dae5c43ff0e70e3afc8be1bb4947e8484254
Paul Antoine <e85555@etu.univ-lorraine.fr>
2025-11-12 09:04:44 +0100

    Default

--- NetVOD/src/Service/action/DefaultAction.php
+++ NetVOD/src/Service/action/DefaultAction.php
@@ -7,7 +7,6 @@ class DefaultAction extends Action
 {
     public function getResult(): string
     {
-        // --- Si l'utilisateur n'est pas connecté ---
         if (!isset($_SESSION['user'])) {
             return <<<HTML
             <div class="center-message">
@@ -22,7 +21,6 @@ HTML;
 
         $idProfil = $_SESSION['profil']['id_profil'] ?? null;
 
-        // --- Si l'utilisateur n'a pas de profil ---
         if (!$idProfil) {
             return <<<HTML
             <div class="center-message">
@@ -37,22 +35,43 @@ HTML;
 
         $pdo = DeefyRepository::getInstance()->getPDO();
 
-        // --- Calcul dynamique du chemin des images ---
         $baseUrl = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
         $imgPrefix = $baseUrl . '/img/';
 
-        // --- Épisodes en cours ---
+        // --- Séries terminées (toutes les séries dont tous les épisodes ont été vus) ---
         $stmt = $pdo->prepare("
-            SELECT e.id_episode, e.titre, e.img, s.titre_serie
+            SELECT s.id_serie
+            FROM serie s
+            INNER JOIN episode e ON s.id_serie = e.id_serie
+            LEFT JOIN visionnees v 
+                ON v.id_episode = e.id_episode AND v.id_profil = :id_profil
+            GROUP BY s.id_serie
+            HAVING COUNT(DISTINCT e.id_episode) = COUNT(DISTINCT v.id_episode)
+        ");
+        $stmt->execute(['id_profil' => $idProfil]);
+        $seriesTerminees = array_column($stmt->fetchAll(), 'id_serie');
+
+        // --- Épisodes en cours (on exclut les séries terminées) ---
+        $stmt = $pdo->prepare("
+            SELECT e.id_episode, e.titre, e.img, s.titre_serie, s.id_serie
             FROM en_cours ec
             JOIN episode e ON ec.id_episode = e.id_episode
             JOIN serie s ON e.id_serie = s.id_serie
             WHERE ec.id_profil = :id_profil
+              AND s.id_serie NOT IN (
+                    SELECT s2.id_serie
+                    FROM serie s2
+                    INNER JOIN episode e2 ON s2.id_serie = e2.id_serie
+                    LEFT JOIN visionnees v2 
+                        ON v2.id_episode = e2.id_episode AND v2.id_profil = :id_profil
+                    GROUP BY s2.id_serie
+                    HAVING COUNT(DISTINCT e2.id_episode) = COUNT(DISTINCT v2.id_episode)
+              )
         ");
         $stmt->execute(['id_profil' => $idProfil]);
         $episodes = $stmt->fetchAll();
 
-        // --- Favoris ---
+        // --- Séries favorites ---
         $stmt = $pdo->prepare("
             SELECT s.id_serie, s.titre_serie, s.img
             FROM favoris f
@@ -62,53 +81,47 @@ HTML;
         $stmt->execute(['id_profil' => $idProfil]);
         $favoris = $stmt->fetchAll();
 
-        // --- Total épisodes ---
+        // --- Séries déjà visionnées (toutes terminées) ---
         $stmt = $pdo->prepare("
-            SELECT 
-                COUNT(DISTINCT e.id_episode) AS total_episodes,
-                e.id_serie,
-                s.titre_serie,
-                s.img
-            FROM episode e
-            INNER JOIN serie s 
-                ON s.id_serie = e.id_serie
-            GROUP BY e.id_serie,s.titre_serie,s.img
+            SELECT s.id_serie, s.titre_serie, s.img
+            FROM serie s
+            WHERE s.id_serie IN (
+                SELECT s2.id_serie
+                FROM serie s2
+                INNER JOIN episode e2 ON s2.id_serie = e2.id_serie
+                LEFT JOIN visionnees v2 
+                    ON v2.id_episode = e2.id_episode AND v2.id_profil = :id_profil
+                GROUP BY s2.id_serie
+                HAVING COUNT(DISTINCT e2.id_episode) = COUNT(DISTINCT v2.id_episode)
+            )
         ");
-        $stmt->execute();
-        $totalep = $stmt->fetchAll();
+        $stmt->execute(['id_profil' => $idProfil]);
+        $seriesVisionnees = $stmt->fetchAll();
 
-        // --- Déjà visionnées ---
-        $stmt = $pdo->prepare("
-            SELECT 
-                e.id_serie,
-                COUNT(DISTINCT v.id_episode) AS episodes_vus
-            FROM episode e
-            INNER JOIN visionnees v 
-                ON v.id_episode = e.id_episode
-            WHERE v.id_profil = ?
-            GROUP BY e.id_serie
-        ");
-        $stmt->execute([$idProfil]);
-        $vision = $stmt->fetchAll();
-
+        // --- HTML ---
         $html = "<h2>Bienvenue, " . htmlspecialchars($_SESSION['user']['email'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . " !</h2>";
 
-        // --- Affichage épisodes en cours ---
+        // --- Épisodes en cours ---
         $html .= "<h3>Épisodes en cours :</h3><div class='series-grid'>";
-        foreach ($episodes as $ep) {
-            $img = htmlspecialchars($ep['img'] ?? 'a.jpg', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-            $titreSerie = htmlspecialchars($ep['titre_serie'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-            $titreEp = htmlspecialchars($ep['titre'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-            $html .= "<div class='serie-card'>
-                        <a href='?action=AfficherEpisode&id={$ep['id_episode']}'>
-                            <img src='{$imgPrefix}{$img}' class='serie-img' alt='{$titreSerie} - {$titreEp}'>
-                        </a>
-                        <a href='?action=AfficherEpisode&id={$ep['id_episode']}'>{$titreSerie} - {$titreEp}</a>
-                      </div>";
+        if (empty($episodes)) {
+            $html .= "<p>Aucun épisode en cours.</p>";
+        } else {
+            foreach ($episodes as $ep) {
+                $img = htmlspecialchars($ep['img'] ?? 'a.jpg', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+                $titreSerie = htmlspecialchars($ep['titre_serie'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+                $titreEp = htmlspecialchars($ep['titre'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+                // lien direct vers l’épisode
+                $html .= "<div class='serie-card'>
+                            <a href='?action=AfficherEpisode&id={$ep['id_episode']}'>
+                                <img src='{$imgPrefix}{$img}' class='serie-img' alt='{$titreSerie} - {$titreEp}'>
+                            </a>
+                            <a href='?action=AfficherEpisode&id={$ep['id_episode']}'>{$titreSerie} - {$titreEp}</a>
+                          </div>";
+            }
         }
         $html .= "</div>";
 
-        // --- Affichage favoris ---
+        // --- Séries favorites ---
         $html .= "<h3>Séries favorites :</h3><div class='series-grid'>";
         foreach ($favoris as $f) {
             $img = htmlspecialchars($f['img'] ?? 'a.jpg', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
@@ -125,21 +138,17 @@ HTML;
         }
         $html .= "</div>";
 
-        // --- Affichage déjà visionnées ---
+        // --- Séries déjà visionnées ---
         $html .= "<h3>Séries déjà visionnées :</h3><div class='series-grid'>";
-        foreach ($totalep as $te) {
-            foreach ($vision as $v) {
-                if ($te['id_serie'] == $v['id_serie'] && $te['total_episodes'] == $v['episodes_vus']) {
-                    $img = htmlspecialchars($te['img'] ?? 'a.jpg', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-                    $titreSerie = htmlspecialchars($te['titre_serie'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-                    $html .= "<div class='serie-card'>
-                                <a href='?action=AfficherSerie&id={$te['id_serie']}'>
-                                    <img src='{$imgPrefix}{$img}' class='serie-img' alt='{$titreSerie}'>
-                                </a>
-                                <a href='?action=AfficherSerie&id={$te['id_serie']}'>{$titreSerie}</a>
-                              </div>";
-                }
-            }
+        foreach ($seriesVisionnees as $sv) {
+            $img = htmlspecialchars($sv['img'] ?? 'a.jpg', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+            $titreSerie = htmlspecialchars($sv['titre_serie'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+            $html .= "<div class='serie-card'>
+                        <a href='?action=AfficherSerie&id={$sv['id_serie']}'>
+                            <img src='{$imgPrefix}{$img}' class='serie-img' alt='{$titreSerie}'>
+                        </a>
+                        <a href='?action=AfficherSerie&id={$sv['id_serie']}'>{$titreSerie}</a>
+                      </div>";
         }
         $html .= "</div>";
 
